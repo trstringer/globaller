5f1407c18b0ed38b0318f165198759cc3db73446
Thomas Stringer <tr.stringer@outlook.com>
2016-08-08 12:32:31 -0400

    add functionality to give necessary permissions

--- src/globaller.js
+++ src/globaller.js
@@ -1,6 +1,8 @@
 const program = require('commander');
 const package = require('../package');
 const chalk = require('chalk');
+const fs = require('fs');
+const exec = require('child_process').exec;
 
 function displaySuccess(message) {
   console.log(chalk.green(message));
@@ -23,28 +25,62 @@ function importPackageJson() {
   return require(`${cwd}/package.json`);
 }
 
+function globalPath() {
+  // currently only support for Linux and macOS
+  return '/usr/local/bin';
+}
+
+function createSymLink(packageBin) {
+  return new Promise((resolve, reject) => {
+    const newBinName = `${packageBin.name}-${program.postfix || 'dev'}`;
+    const newBinAndPath = `${globalPath()}/${newBinName}`;
+    fs.symlink(packageBin.path, newBinAndPath, (err) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        // unfortunately we have to use exec() here as fs.chmod() 
+        // is not seemingly setting the appropriate permissions
+        exec(`chmod 755 ${newBinAndPath}`, (err) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            displayVerbose(`sucess chmod 755 on ${newBinAndPath}`);
+            resolve(newBinName);
+          }
+        });
+      }
+    });
+  });
+}
+
 function binConfig(packageJson) {
   let bin = {};
   if (typeof packageJson.bin === 'object') {
     // at this point the format of bin is 
     // { "binname": "binpath" }
-    bin = packageJson.bin;
-    const binRelPath = bin[Object.keys(bin)[0]]; 
-    bin[Object.keys(bin)[0]] = `${process.cwd()}/${binRelPath}`;
+    const binRelPath = packageJson.bin[Object.keys(packageJson.bin)[0]]; 
+
+    bin.name = Object.keys(packageJson.bin)[0];
+    bin.path = `${process.cwd()}/${binRelPath}`;
   }
   else {
     // the bin is a string with the path and 
     // we need to use the package name as the bin name
-    bin[packageJson.name] = `${process.cwd()}/${packageJson.bin}`
+    bin.name = packageJson.name;
+    bin.path = `${process.cwd()}/${packageJson.bin}`;
   }
 
-  displayVerbose(`transformed bin ${bin}`);
+  displayVerbose(`transformed bin '${bin.name} : ${bin.path}'`);
+
+  return bin;
 }
 
 program
   .version(package.version)
   .option('-v, --verbose', 'display verbose logging')
-  .option('-p, --postfix', 'add non-default postfix to global module (default is "-dev")')
+  .option('-p, --postfix [postfix]', 'add non-default postfix to global module (default is "-dev")')
   .parse(process.argv);
 
 try {
@@ -55,8 +91,19 @@ try {
     displayError('no bin indicator in package json');
     process.exit(1);
   }
+
+  const packageBin = binConfig(packageJson);
+  createSymLink(packageBin)
+    .then((newBinName) => {
+      displaySuccess(`successfully created symlink '${newBinName}'`);
+      process.exit(0);
+    })
+    .catch((err) => {
+      displayError(err.message);
+      process.exit(1);
+    });
 }
 catch (ex) {
-  displayError('unable to access package json');
+  displayError(ex.message);
   process.exit(1);
 }
